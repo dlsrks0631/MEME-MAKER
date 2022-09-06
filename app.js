0034579841820200a8b5ff441c35d11a77a35dc6
dlsrks0631 <65211547+dlsrks0631@users.noreply.github.com>
2022-09-06 23:54:24 +0900

    Update Summary

--- app.js
+++ app.js
@@ -3,4 +3,9 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(50, 50, 100, 200);
+ctx.fillRect(200, 200, 15, 100);
+ctx.fillRect(360, 200, 15, 100);
+ctx.fillRect(260, 200, 60, 200);
+
+ctx.arc(290, 150, 50, 0, 2 * Math.PI);
+ctx.fill();
