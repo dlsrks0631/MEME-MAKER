83c9c325c025e293bcdf5191c8a7dadd2849f18c
dlsrks0631 <65211547+dlsrks0631@users.noreply.github.com>
2022-09-07 14:09:47 +0900

    Update page

--- app.js
+++ app.js
@@ -1,11 +1,60 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const lineWidth = document.getElementById("line-width");
+const color = document.getElementById("color");
+const colorOptions = Array.from(
+  document.getElementsByClassName("color-option")
+);
+
 canvas.width = 800;
 canvas.height = 800;
+ctx.lineWidth = lineWidth.value;
 
-ctx.fillRect(200, 200, 15, 100);
-ctx.fillRect(360, 200, 15, 100);
-ctx.fillRect(260, 200, 60, 200);
+const colors = ["red", "blue", "purple"];
+let isPainting = false;
 
-ctx.arc(290, 150, 50, 0, 2 * Math.PI);
-ctx.fill();
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.beginPath(); // 이전에 그려진 선과 새로운 선의 연결을 끊어내기 위함
+  ctx.moveTo(event.offsetX, event.offsetY);
+}
+
+function startPainting() {
+  // 선을 그리게 함
+  isPainting = true;
+}
+
+function cancelPainting() {
+  // 마우스 포인터만 움직임
+  isPainting = false;
+}
+
+function onLineWidthChange(event) {
+  ctx.lineWidth = event.target.value;
+}
+
+function onColorChange(event) {
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
+}
+
+function onColorClick(event) {
+  const colorValue = event.target.dataset.color;
+  ctx.strokeStyle = colorValue;
+  ctx.fillStyle = colorValue;
+  color.value = colorValue;
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
+
+lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
+
+colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
